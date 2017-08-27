dd5225751cf0143995ca151326236e0c35871350
Hans Kurniadi <hans.d.k@live.com>
2017-08-27 16:26:09 -0700

    moved navbar to a separate component

--- src/components/App.js
+++ src/components/App.js
@@ -1,119 +1,16 @@
 import React, { Component } from 'react';
 // import logo from './logo.svg';
-import '../stylesheets/App.css';
-import AppBar from 'material-ui/AppBar';
-import Drawer from 'material-ui/Drawer';
-import Popover from 'material-ui/Popover';
-import Menu from 'material-ui/Menu';
-import MenuItem from 'material-ui/MenuItem';
-import Dialog from 'material-ui/Dialog';
-import RaisedButton from 'material-ui/RaisedButton';
 
+import NavBar from './NavBar';
 import Welcome from './Welcome';
 import Events from './Events';
 
 class App extends Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      // drawerIsOpen: false,
-      popoverIsOpen: false,
-      targetOrigin: {
-        horizontal: 'left',
-        vertical: 'top'
-      },
-      // welcomeModalIsOpen: true
-    }
-  }
-  
-  // toggleAppBarDrawer = (event) => {
-  //   event.preventDefault();
-  //   this.setState({
-  //     drawerIsOpen: !this.state.drawerIsOpen
-  //   })
-  // };
-
-  toggleAppBarPopover = (event) => {
-    event.preventDefault();
-    // console.log(event.currentTarget);
-    this.setState({
-      popoverIsOpen: !this.state.popoverIsOpen,
-      anchorEl: event.currentTarget
-    })
-  }
-  
-  closePopover = (event) => {
-    this.setState({
-      popoverIsOpen: false
-    })
-  }
-
-  // closeWelcomeModal = (event) => {
-  //   this.setState({
-  //     welcomeModalIsOpen: false
-  //   })
-  // }
-
   render() {
-    const menuStyle = {
-      textAlign: 'left'
-    };
-
-    const popoverStyle = {
-      width: '100%',
-      textAlign: 'center'
-    };
-
-    // const welcomeModalActions = [
-    //   <RaisedButton 
-    //     label="Discover Mudika"
-    //     modal={true}
-    //     onClick={this.closeWelcomeModal}
-    //     primary={true}
-    //   />
-    // ]
-
     return (
       <div className="App">
-        {/* <Dialog
-          open={this.state.welcomeModalIsOpen}
-          actions={welcomeModalActions}
-        >
-          Welcome to Mudika Vancouver!
-        </Dialog> */}
-        <AppBar 
-          title='Mudika Vancouver'
-          // onLeftIconButtonTouchTap={this.toggleAppBarDrawer}
-          onTitleTouchTap={this.toggleAppBarPopover}
-          showMenuIconButton={false}
-        />
-        <Popover
-          open={this.state.popoverIsOpen}
-          onRequestClose={this.closePopover}
-          animated={true}
-          anchorEl={this.state.anchorEl}
-          style={popoverStyle}
-          targetOrigin={this.state.targetOrigin}
-        >
-          <Menu>
-            <MenuItem>News</MenuItem>
-            <MenuItem>Ministries</MenuItem>
-          </Menu>
-        </Popover>
-
-        <Welcome></Welcome>
-        {/* <Events></Events> */}
-        {/* <Drawer
-          docked={false}
-          width={200}
-          open={this.state.drawerIsOpen}
-          onRequestChange={ (open) => this.setState({drawerIsOpen: open}) }
-         >
-          <Menu style={menuStyle}>
-            <MenuItem>News</MenuItem>
-            <MenuItem>Ministries</MenuItem>
-          </Menu>
-        </Drawer> */}
+        <NavBar />
+        <Welcome />
       </div>
     );
   }
