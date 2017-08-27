721c4c74fb7c5b9c9363dc4798ed10e4a782448a
Hans Kurniadi <hans.d.k@live.com>
2017-08-26 23:54:56 -0700

    changed drawer to popover in appbar

--- src/components/App.js
+++ src/components/App.js
@@ -3,41 +3,90 @@ import React, { Component } from 'react';
 import '../stylesheets/App.css';
 import AppBar from 'material-ui/AppBar';
 import Drawer from 'material-ui/Drawer';
+import Popover from 'material-ui/Popover';
+import Menu from 'material-ui/Menu';
 import MenuItem from 'material-ui/MenuItem';
 
 class App extends Component {
   constructor(props) {
     super(props);
     this.state = {
-      drawerIsOpen: false
+      // drawerIsOpen: false,
+      popoverIsOpen: false,
+      targetOrigin: {
+        horizontal: 'left',
+        vertical: 'top'
+      }
     }
-
-    this.toggleAppBarDrawer = this.toggleAppBarDrawer.bind(this);
+    // Functions 'this' binding
+    // this.toggleAppBarDrawer = this.toggleAppBarDrawer.bind(this);
+    this.toggleAppBarPopover = this.toggleAppBarPopover.bind(this);
   }
   
-  toggleAppBarDrawer = (event) => {
+  // toggleAppBarDrawer = (event) => {
+  //   event.preventDefault();
+  //   this.setState({
+  //     drawerIsOpen: !this.state.drawerIsOpen
+  //   })
+  // };
+
+  toggleAppBarPopover = (event) => {
     event.preventDefault();
+    // console.log(event.currentTarget);
     this.setState({
-      drawerIsOpen: !this.state.drawerIsOpen
+      popoverIsOpen: !this.state.popoverIsOpen,
+      anchorEl: event.currentTarget
     })
-  };
+  }
   
+  handleClosePopover = (event) => {
+    this.setState({
+      popoverIsOpen: false
+    })
+  }
+
   render() {
+    const menuStyle = {
+      textAlign: 'left'
+    };
+
+    const popoverStyle = {
+      width: '100%',
+      textAlign: 'center'
+    };
+
     return (
       <div className="App">
         <AppBar 
           title='Mudika Vancouver'
-          onLeftIconButtonTouchTap={this.toggleAppBarDrawer}
+          // onLeftIconButtonTouchTap={this.toggleAppBarDrawer}
+          onTitleTouchTap={this.toggleAppBarPopover}
+          showMenuIconButton={false}
         />
-        <Drawer
+        <Popover
+          open={this.state.popoverIsOpen}
+          onRequestClose={this.handleClosePopover}
+          anchorEl={this.state.anchorEl}
+          style={popoverStyle}
+          targetOrigin={this.state.targetOrigin}
+        >
+          <Menu>
+            <MenuItem>News</MenuItem>
+            <MenuItem>Ministries</MenuItem>
+          </Menu>
+        </Popover>
+
+        {/* <Drawer
           docked={false}
           width={200}
           open={this.state.drawerIsOpen}
           onRequestChange={ (open) => this.setState({drawerIsOpen: open}) }
-        >
-          <MenuItem>News</MenuItem>
-          <MenuItem>Ministries</MenuItem>
-        </Drawer>
+         >
+          <Menu style={menuStyle}>
+            <MenuItem>News</MenuItem>
+            <MenuItem>Ministries</MenuItem>
+          </Menu>
+        </Drawer> */}
       </div>
     );
   }
